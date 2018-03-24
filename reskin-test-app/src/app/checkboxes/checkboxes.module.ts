b97b82577f6f9c67e000e0a36589d33901d28fac
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2018-03-24 15:43:09 -0700

    Fixed the material theming issue

--- reskin-test-app/src/app/checkboxes/checkboxes.module.ts
+++ reskin-test-app/src/app/checkboxes/checkboxes.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CheckboxComponent } from './checkbox/checkbox.component';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { ThemingModule } from '../theming/theming.module';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
     CommonModule,
     MatCheckboxModule,
-    MatProgressBarModule,
     ThemingModule
   ],
   declarations: [CheckboxComponent],
