c2874feddeb1acf9aaf3b93ede7c09ac56f2c8d0
David Leblanc <David.LeBlanc@hamiltoncompany.com>
2018-04-09 20:41:47 -0700

    Implemented forward and back styles for chevrons

--- reskin-test-app/src/app/buttons/chevron-button/chevron-button.component.ts
+++ reskin-test-app/src/app/buttons/chevron-button/chevron-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ElementRef } from '@angular/core';
 import { ColorPalette } from '../../theming/color-palette.enum';
 import { Guid } from '../../theming/guid';
 
@@ -9,12 +9,26 @@ import { Guid } from '../../theming/guid';
 })
 export class ChevronButtonComponent implements OnInit {
 
-  private uniqueId = Guid.newGuid();
-  private getUniqueIdUrl() {
-    return 'url(#' + this.uniqueId + ')';
+  private fillId = Guid.newGuid();
+  private getFillIdUrl() {
+    return 'url(#' + this.fillId + ')';
+  }
+  private pathId = Guid.newGuid();
+  private getPathIdUrl() {
+    return 'url(#' + this.pathId + ')';
   }
   @Input() color: ColorPalette;
-  constructor() {
+  private advance: boolean;
+
+  constructor(private elementRef: ElementRef) {
+    this.advance = true;
+    if(elementRef.nativeElement.hasAttribute('hx-chevron-back')) {
+      this.advance = false;
+    }
+    else if(elementRef.nativeElement.hasAttribute('hx-chevron-next')) {
+      this.advance = true;
+    }
+    //attributes.some(attribute => this._getHostElement().hasAttribute(attribute));
     this.color = ColorPalette.Default;
    }
 
